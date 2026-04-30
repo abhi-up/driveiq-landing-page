1d880653cf51957e4579b4d51648cbae2915055f
Abhishek Upadhyay <77201101+abhi-up@users.noreply.github.com>
2026-04-30 15:27:33 +0530

    Hide Powered By Section in About page

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -129,7 +129,7 @@ const About = () => {
         </section>
 
         {/* Powered By Section */}
-        <section className="section-padding bg-navy-50">
+        <section className="section-padding bg-navy-50 display:none">
           <div className="container-section">
             <div className="grid md:grid-cols-2 items-center gap-12">
               {/* Logo */}
